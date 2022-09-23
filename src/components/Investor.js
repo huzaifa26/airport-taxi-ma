9e401aa0e583a4bfd1e7ab1de323f8fc5da24ca5
huzaifa26 <abidhuzaifa128@gmail.com>
2022-09-24 00:08:09 +0500

    changed investor section font size

--- src/components/Investor.js
+++ src/components/Investor.js
@@ -23,8 +23,8 @@ export default function Investor(props){
             <div className="flex-1 min-w-[301.54px] w-[27.77777777777778vw] h-full xsm:mt-[40px] sm:mt-[40px]">
                 <div className="flex flex-col justify-center min-w-[301.54px] w-[41.80555555555556vw] h-full m-auto">
                     <img className="xsm:hidden sm:hidden block w-[56.05px] h-[33.02px] mb-[43px]" src="./topQuotation.png"/>
-                    <h3 className="text-[clamp(24.04,3.3333333333333335vw,48px)] text-[#FFB000] xsm:leading-[28.1px] sm:leading-[28.1px] leading-[56px] font-[800]">Absolutely wonderful!</h3>
-                    <p className="xsm:mt-[10px] sm:mt-[10px] mt-[30px] text-[clamp(12.52,1.7361111111111112vw,25px)] text-[#656565] xsm:leading-[23px] sm:leading-[23px] leading-[46px] font-[400] space-[10px]">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
+                    <h3 className="text-[clamp(24.04px,3.3333333333333335vw,48px)] text-[#FFB000] xsm:leading-[28.1px] sm:leading-[28.1px] leading-[56px] font-[800]">Absolutely wonderful!</h3>
+                    <p className="xsm:mt-[10px] sm:mt-[10px] mt-[30px] text-[clamp(12.52px,1.7361111111111112vw,25px)] text-[#656565] xsm:leading-[23px] sm:leading-[23px] leading-[46px] font-[400] space-[10px]">In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</p>
                     <img className="self-end xsm:w-[28.08px] xsm:h-[14.45px] sm:w-[28.08px] sm:h-[14.45px] w-[56.05px] h-[33.02px] mt-[13px]" src="./bottomQuotation.png"/>
                     <div className="mt-[70px] self-end">
                         <ul className="xsm:hidden sm:hidden flex items-center gap-[20px] text-[22px] font-[400]">
